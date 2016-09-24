51180a7d205648f71b23d60b76bf3f3aa7947c18
Charles J Hardy <chuckjhardy@gmail.com>
2016-09-24 12:33:49 +0100

    Result Object

---
+++ lib/postcodesio/result.rb
@@ -0,0 +1,23 @@
+class Postcodesio
+  class Result
+    attr_reader :response
+
+    def initialize(response:)
+      @response = response
+
+      build
+    end
+
+    def build
+      response.each(&define_method_for_header)
+    end
+
+    alias as_hash response
+
+    private
+
+    def define_method_for_header
+      ->(key, value) { self.class.send(:define_method, key) { value } }
+    end
+  end
+end
---
+++ spec/postcodeio/result_spec.rb
@@ -0,0 +1,61 @@
+require "spec_helper"
+
+def response
+  {
+    postcode: "EX17 4AZ",
+    quality: 1,
+    eastings: 287994,
+    northings: 104277,
+    country: "England",
+    nhs_ha: "South West",
+    longitude: -3.59168076007149,
+    latitude: 50.8271675496744,
+    parliamentary_constituency: "Central Devon",
+    european_electoral_region: "South West",
+    primary_care_trust: "Devon",
+    region: "South West",
+    lsoa: "Mid Devon 010A",
+    msoa: "Mid Devon 010",
+    incode: "4AZ",
+    outcode: "EX17",
+    distance: 1.732027679,
+    admin_district: "Mid Devon",
+    parish: "Stockleigh Pomeroy",
+    admin_county: "Devon",
+    admin_ward: "Newbrooke",
+    ccg: "NHS North, East, West Devon",
+    nuts: "Devon CC",
+    codes: {
+      admin_district: "E07000042",
+      admin_county: "E10000008",
+      admin_ward: "E05008547",
+      parish: "E04003050",
+      ccg: "E38000129",
+      nuts: "UKK43"
+    }
+  }
+end
+
+RSpec.describe Postcodesio::Result do
+  let(:instance) do
+    described_class.new(response: response)
+  end
+
+  describe "#as_hash" do
+    subject(:as_hash) { instance.as_hash }
+
+    it "returns response" do
+      expect(as_hash).to eq(response)
+    end
+  end
+
+  response.each do |key, value|
+    describe "##{key}" do
+      subject(:action) { instance.public_send(key) }
+
+      it "returns expected value" do
+        expect(action).to eq(value)
+      end
+    end
+  end
+end
